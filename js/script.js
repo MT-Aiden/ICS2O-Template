3e57ad0916055d4df32d8f2ad025056c29dac3da
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-04-04 12:49:37 -0400

    d js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// Created by: Aiden McLeod
-// Created on: Mar 2022
-// This file contains the JS functions for index.html
-
-function myButtonClicked() {
-    // this function displays "Hello, World!"
-    document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
-  }
-  
